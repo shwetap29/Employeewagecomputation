61ca74e6b8edcf3e2fd793e6dc6a9d7062f6476c
shwetap29 <shwetaprasad48@gmail.com>
2021-05-10 09:51:07 +0530

    added wagesofhours in Employeewagecomputation.sh

--- Employeewagecomputation.sh
+++ Employeewagecomputation.sh
@@ -5,23 +5,52 @@ WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
 PART_TIME_HOUR=4
 IS_PRESENT=1
-empcheck=$((RANDOM%3+1))
+#empcheck=$((RANDOM%3+1))
+NUMBEROFWORKINGDAYS=20
+countFullTime=0
+countPartTime=0
+countNotPresent=0
+for(( day=1; day <= $NUMBEROFWORKINGDAYS ; day++ ))
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
+PART_TIME_HOUR=4
+NUMBER_OF_HOUR=100
+NUMBER_OF_DAYS=20
+#variable
+numberOfDays=20
+totalWorkingDays=0
+totalEmpHrs=0
+#calculate wages till a condition
+#case $empcheck in
+while(( $NUMBER_OF_HOUR > $totalEmpHrs  && $NUMBER_OF_DAYS > $totalWorkingDays  ))
+do
+	totalWorkingDays=$((totalWorkingDays+1))
+	empcheck=$((RANDOM%3+1))
+	#Using case statement
+	case $random in
+		1)
+			calculateDailyEmployeewage=$(( WAGE_PER_HOUR * FULLDAYHOUR ))				#Calcute full time
+			countFullTime=$((countFullTime+1))
+			empHrs=8						 #Calcute full time
+			;;
 
-#using case statement
-case $empcheck in
-     1) 
-             echo "Employee is present\n"
-             calculateDailyEmployeewage=$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
-             echo "Full time : $calculateDailyEmployeewage \n"
-              ;;
+		2)
+			calculatePartTime=$((WAGE_PER_HOUR * PARTTIMEHOUR ))						 #Calculate part time
+			countPartTime=$((countPartTime+1))
+			empHrs=4						 #Calculate part time
+			;;
 
-      2)
-            echo "Employee is present\n"
-         calculateparttime=$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
-        echo "part time employee wage : $calculateparttime \n"
-          ;;     
-      3) 
-           echo "Employee is absent"
-             ;;
-esac
+		3)
+			countNotPresent=$((countNotPresent+1))
+			empHrs=0
+			;;
+	esac
+done
+echo "Full time $countFullTime days employee wage :$(( calculateDailyEmployeewage*$countFullTime ))"
+echo "Part time $countPartTime days employee wage :$(( calculatePartTime*$countPartTime ))"
+echo "Not present $countNotPresent days"
 
+totalEmpHrs=$(($totalEmpHrs+$empHrs))	# Total employee hour
+done
+totalSalary=$(($totalEmpHrs*$numberOfDays))
+echo "Total Salary is :$totalSalary"
