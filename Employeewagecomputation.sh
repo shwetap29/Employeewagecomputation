b117d677f6694e49070c39f840510d1db557e506
shwetap29 <shwetaprasad48@gmail.com>
2021-05-14 12:05:19 +0530

    added  using function to get work hours in Employeewagecomputation.sh

--- Employeewagecomputation.sh
+++ Employeewagecomputation.sh
@@ -4,24 +4,38 @@ echo "Welcome to Employee wage computation"
 WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
 PART_TIME_HOUR=4
-IS_PRESENT=1
-empcheck=$((RANDOM%3+1))
+#IS_PRESENT=1
+NUMBER_OF_HOUR=100
+NUMBER_OF_DAYS=20
+#variable
+numberOfDays=20
+totalWorkingDays=0
+totalEmpHrs=0
+#Using function
+function getWorkingHours()
+{
+	random=$1											#Using case statement
+	case $random in
+		1)
+			empHrs=8						 #Calcute full time
+			;;
 
-#using case statement
-case $empcheck in
-     1) 
-             echo "Employee is present\n"
-             calculateDailyEmployeewage=$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
-             echo "Full time : $calculateDailyEmployeewage \n"
-              ;;
+		2)
+			empHrs=4						 #Calculate part time
+			;;
 
-      2)
-            echo "Employee is present\n"
-         calculateparttime=$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
-        echo "part time employee wage : $calculateparttime \n"
-          ;;     
-      3) 
-           echo "Employee is absent"
-             ;;
-esac
+		3)      empHrs=0
+			;;
+	esac
+echo "$empHrs"
+}
+   while(( $NUMBER_OF_HOUR > $totalEmpHrs  && $NUMBER_OF_DAYS > $totalWorkingDays ))
+do
+   totalWorkingDays=$((totalWorkingDays+1))
+   empHrs="$( getWorkingHours $((RANDOM%3+1)) )"
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))	# Total employee hour
+done
+    totalSalary=$(($totalEmpHrs*$numberOfDays))
+    echo "Total Salary is : $totalSalary"
+
 
